df1012a9cbc5eea44ca91af7ed38bd44c9a476eb
leoadams2022 <sam0941535901@gmail.com>
2023-04-28 23:15:48 +0300

    202332

--- public/Product_Page.php
+++ public/Product_Page.php
@@ -9,10 +9,6 @@
     <link rel="stylesheet" href="assets/css/reset.css">
     <!-- Product_Page_style  -->
     <link rel="stylesheet" href="assets/css/Product_Page_style.css">
-    <!-- bootstrap  -->
-    <link rel="stylesheet" href="assets/css/Bootstrap_v5.3.css">
-    <!-- bootstrap  -->
-    <script src="assets/js/Bootstrap_v5.3.js"></script>
     <!-- jQuery  -->
     <script src="assets/js/jQuery.js"></script>
 </head>
@@ -22,18 +18,66 @@
         <div class="nav_warper">
             <h2 class="">Products Add</h2>
             <div class="">
-                <button class='btn delete_btn' onclick="massDelete()">Save</button>
-                <button class='btn delete_btn' onclick="massDelete()">Cancel</button>
+                <button class='btn save_btn' onclick="">Save</button>
+                <button class='btn cancel_btn' onclick="">Cancel</button>
             </div>
         </div>
         <hr>
-    </section>
+</section>
 <!-- // nav_section -->
-<!-- main section -->
-    <section id='' class="main_section container">
+<!-- from_section -->
+<section class='from_section container'>
+    <form action="" id="product_form">
+        <div class="basic_info">
+            <div>
+                <label for="sku" class="input_lable">SKU:</label>
+                <input type="text" id="sku" class="input text_input" name="sku">
+            </div>
+            <div>
+                <label for="name" class="input_lable">Name:</label>
+                <input type="text" id="name" class="input text_input" name="name">
+            </div>
+            <div>
+                <label for="price" class="input_lable">Price:</label>
+                <input type="number" id="price" class="input number_input" name="price">
+            </div>
+            <div>
+                <label for="productType" class="input_lable">Type switcher:</label>
+                <select name="" id="productType" name="productType" class="input productType_input">
+                    <option value="dvd">DVD</option>
+                    <option value="book">Book</option>
+                    <option value="furniture">Furniture</option>
+                </select>
+            </div>
+        </div>
+        
+        <div class="props">
+            <div id="dvd">
+                <label for="size" class="input_lable">Size (MB):</label>
+                <input type="number" id="size" class="input number_input" name="size">
+            </div>
 
-    </section>     
-<!-- // main section -->
+            <div id="book">
+                <label for="weight" class="input_lable">Weight:</label>
+                <input type="number" id="weight" class="input number_input" name="weight">
+            </div>
+
+            <div id="furniture">
+                <label for="height" class="input_lable">Height:</label>
+                <input type="number" id="height" class="input number_input" name="height">
+                
+                <label for="width" class="input_lable">Width:</label>
+                <input type="number" id="width" class="input number_input" name="width">
+
+                <label for="lenght" class="input_lable">Length:</label>
+                <input type="number" id="lenght" class="input number_input" name="lenght">
+            </div>
+        </div>
+        
+    </form>
+</section>
+<!-- // from_section -->
+
 <script>
 
 </script>
